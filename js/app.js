94a1a6e5ad448f43b28b95f63a3837a84ca58181
Carlos Javier Bazan Huaman <pacoyx@hotmail.com>
2021-06-04 15:43:19 -0500

    se agrego funcion javascript para generar csv

--- js/app.js
+++ js/app.js
@@ -74,7 +74,7 @@ function append(parent, el) {
     return parent.appendChild(el);
 }
 
-
+// https://programacion.net/articulo/como_exportar_una_tabla_html_a_csv_mediante_javascript_1742
 function descagar(){
     alert('descargando...');
 }
@@ -140,4 +140,49 @@ function buildTable(data) {
     });
     table.appendChild(tbody);             
     return table;
+}
+
+
+function downloadCSV(csv, filename) {
+    var csvFile;
+    var downloadLink;
+
+    // CSV file
+    csvFile = new Blob([csv], {type: "text/csv"});
+
+    // Download link
+    downloadLink = document.createElement("a");
+
+    // File name
+    downloadLink.download = filename;
+
+    // Create a link to the file
+    downloadLink.href = window.URL.createObjectURL(csvFile);
+
+    // Hide download link
+    downloadLink.style.display = "none";
+
+    // Add the link to DOM
+    document.body.appendChild(downloadLink);
+
+    // Click download link
+    downloadLink.click();
+}
+
+
+function exportTableToCSV(filename) {
+    var csv = [];
+    var rows = document.querySelectorAll("table tr");
+    
+    for (var i = 0; i < rows.length; i++) {
+        var row = [], cols = rows[i].querySelectorAll("td, th");
+        
+        for (var j = 0; j < cols.length; j++) 
+            row.push(cols[j].innerText);
+        
+        csv.push(row.join(";"));        
+    }
+
+    // Download CSV file
+    downloadCSV(csv.join("\n"), filename);
 }
